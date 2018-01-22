e740ec462da79b306ce45f3d3981ada0da5d5040
Brady Madden <bradymadden97@gmail.com>
2018-01-22 20:41:56 +0100

    Create presentation.js

---
+++ presentation.js
@@ -0,0 +1,44 @@
+	(function(){
+		var presentationwrapper = document.createElement("div");
+		var presentationclose = document.createElement("div");
+		var presentationcontainer = document.createElement("div");
+		presentationwrapper.id = "presentation-wrapper";
+		presentationclose.id = "presentation-close";
+		presentationclose.innerHTML = "X";
+		presentationcontainer.id = "presentation-container";
+		presentationwrapper.appendChild(presentationclose);
+		presentationwrapper.appendChild(presentationcontainer);
+		document.getElementsByTagName("BODY")[0].appendChild(presentationwrapper);
+		document.getElementById("presentation-container").addEventListener('click', function(e){
+			if(e.target.tagName.toLowerCase() != "img"){
+				presentationClose();
+			}else{
+				e.stopPropagation();
+			}
+		});
+		document.getElementById("presentation-close").addEventListener('click', function(){
+			presentationClose();
+		});
+		var allimgs = document.getElementsByTagName("img");
+		for(var i = 0; i < allimgs.length; i++){
+			allimgs[i].addEventListener('click', function(){
+				presentationMode(this);
+			});
+		}
+	})();
+	function presentationClose(){
+		document.getElementsByTagName("BODY")[0].style.overflow = "auto";
+		document.getElementById("presentation-wrapper").classList.remove("presentation-show");
+		document.getElementById("presentation-container").innerHTML = "";
+	}
+	function presentationMode(img){
+		var showimg = document.createElement("img");
+		showimg.src = img.src;
+		showimg.height = img.naturalHeight;
+		showimg.width = img.naturalWidth;
+		showimg.classList.add("presentation-image");
+		document.getElementsByTagName("BODY")[0].style.overflow = "hidden";
+		document.getElementById("presentation-wrapper").classList.add("presentation-show");
+		document.getElementById("presentation-container").appendChild(showimg);	
+	}
+	
